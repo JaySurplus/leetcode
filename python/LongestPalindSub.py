a53536dcbdf123f588e86c5be5fdda5473f09789
JaySurplus <znznbest2004@gmail.com>
2015-04-25 12:56:07 -0500

    happpyNum Done. Longest Palindorm substring not done.

---
+++ python/LongestPalindSub.py
@@ -0,0 +1,15 @@
+class Solution:
+    # @param {string} s
+    # @return {string}
+    def longestPalindrome(self, s):
+    	
+
+
+
+
+
+
+s = 'abcdcdfadfae'
+
+test = Solution()
+print test.longestPalindrome(s)
