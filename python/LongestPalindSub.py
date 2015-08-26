8dfa599340f206780789631d6ba4e953c18ba0f4
JaySurplus <znznbest2004@gmail.com>
2015-08-26 04:29:57 -0500

    Looking for linear solution for longestPladinSubstring

--- python/LongestPalindSub.py
+++ python/LongestPalindSub.py
@@ -3,13 +3,37 @@ class Solution:
     # @return {string}
     def longestPalindrome(self, s):
     	self.s = s
-    	for      
+    	longest = ''
+    	
+    	for i in range(len(self.s)):
+    		k = 1
+    		if len(s[i]) > len(longest):
+    			longest = s[i] 
+    		while i - k >=0 and i+k < len(self.s) and len(longest) < (len(self.s)-i)*2:
+    			#print i, k ,longest
+    			if s[i-k] == s[i+k]:
+    				if 2*k +1> len(longest):
+    					longest = s[i-k:i+k+1]
+    				k+=1
+    			else :
+    				break
+    		k = 1
+    		while i -k +1>= 0 and i+k < len(self.s) and len(longest) < (len(self.s)-i)*2:
+    			if s[i-k+1] == s[i+k]:
+    				if 2*k> len(longest):
+    					longest = s[i-k+1:i+k+1]
+    				k+=1
+    			else :
+    				break
+    	return longest
+
 
 
 
 
 def main():
-	s = 'abcdcdfadfae'
+	s = "abbba"
+
 	sol = Solution()
 
 	print sol.longestPalindrome(s)
