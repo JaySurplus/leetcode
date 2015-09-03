0e0168ddd9415152b27e37c9099e2f6eb21c0d3e
JaySurplus <znznbest2004@gmail.com>
2015-09-03 00:44:07 -0500

    add reverseInteger.cpp and template.cpp

---
+++ cpp/reverseInteger.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+
+class Solution {
+public:
+    int reverse(int x) {
+       
+           int newN =0, left =0;  
+            while(x != 0)  
+            {  
+                 if (newN>214748364 || newN<-214748364) {
+                     return 0;
+                     break;
+                 }
+                 left = x%10;  
+                 newN = newN*10 + left;  
+                 x = x/10;  
+            }  
+            return newN;  
+            
+    }
+};
+
+
+
+int main () {
+  Solution test;
+  
+  cout << "result: " << test.reverse(12315312) << endl;
+
+  return 0;
+}
