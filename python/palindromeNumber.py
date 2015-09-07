ac8a5226758eb2870d96ad8a7af5917f76263da7
JaySurplus <znznbest2004@gmail.com>
2015-09-07 01:21:43 -0500

    Init PalindromeNumber.py

--- python/palindromeNumber.py
+++ python/palindromeNumber.py
@@ -2,3 +2,11 @@
 	Palindrome Number
 	https://leetcode.com/problems/palindrome-number/
 """
+
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        
