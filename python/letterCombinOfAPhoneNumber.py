be8d2448ac717590db631549a45c00d80e3aec3a
JaySurplus <znznbest2004@gmail.com>
2015-12-16 01:20:26 -0600

    Start Letter Combination of a phone number

---
+++ python/letterCombinOfAPhoneNumber.py
@@ -0,0 +1,20 @@
+"""
+	Letter Combinations of a Phone Number
+
+	Given a digit string, return all possible letter combinations that the number could represent.
+
+
+	A mapping of digit to letters (just like on the telephone buttons) is given below.
+
+
+	https://leetcode.com/problems/letter-combinations-of-a-phone-number/
+"""
+
+
+class Solution(object):
+    def letterCombinations(self, digits):
+        """
+        :type digits: str
+        :rtype: List[str]
+        """
+        
