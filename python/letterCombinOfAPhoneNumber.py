d135aaba5094164d1d8215314a2e600211a507f6
JaySurplus <znznbest2004@gmail.com>
2015-12-17 10:51:30 -0600

    Letter Combinations of A phone number is done. "

--- python/letterCombinOfAPhoneNumber.py
+++ python/letterCombinOfAPhoneNumber.py
@@ -10,11 +10,38 @@
 	https://leetcode.com/problems/letter-combinations-of-a-phone-number/
 """
 
-
+import copy
 class Solution(object):
     def letterCombinations(self, digits):
         """
         :type digits: str
         :rtype: List[str]
         """
-        
+        self.numAlpha = {"2":['a','b','c'],
+        				 "3":['d','e','f'],
+        				 "4":['g','h','i'],
+        				 "5":['j','k','l'],
+        				 "6":['m','n','o'],
+        				 "7":['p','q','r','s'],
+        				 "8":['t','u','v'],
+        				 "9":['w','x','y','z'],
+        				 "0":[' ']}
+        if len(digits) == 0:
+        	return []
+
+        def letterComb(s , l):
+        	return map(lambda x:x+l,s)
+
+        result = self.numAlpha[digits[0]]
+
+        for i in range(1,len(digits)):
+        	temp = []
+        	for l in self.numAlpha[digits[i]]:
+        		temp += letterComb(result, l)
+        	result = temp
+        	
+        return result
+	
+
+sol = Solution()
+print sol.letterCombinations("")
