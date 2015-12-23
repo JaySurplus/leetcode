603a0c958c9eb065a73126742598ad14804600e0
JaySurplus <znznbest2004@gmail.com>
2015-12-23 01:08:10 -0600

    Update generateParentheses.py

--- python/generateParentheses.py
+++ python/generateParentheses.py
@@ -15,31 +15,34 @@ import time
 
 
 
-class Solution:
-    # @param an integer
-    # @return a list of string
-    # @draw a decision tree when n == 2, and you can understand it!
-    def helpler(self, l, r, item, res):
-        if r < l:
-            return
-        if l == 0 and r == 0:
-            res.append(item)
-        if l > 0:
-            self.helpler(l - 1, r, item + '(', res)
-        if r > 0:
-            self.helpler(l, r - 1, item + ')', res)
-    
+class Solution(object):
     def generateParenthesis(self, n):
-        if n == 0:
-            return []
-        res = []
-        self.helpler(n, n, '', res)
-        return res		
+		"""
+		:type n: int
+		:rtype: List[str]
+		"""
+		if n == 0:
+		    return []
+		finalSol = []
+        	
+		self.parenthesisRecursive( n , n , '' , finalSol)
+		return finalSol
+
+
+    def parenthesisRecursive(self, l ,r ,  sol , finalSol ):
+	    if r < l:
+	        return
+	    if l == 0 and r == 0:
+	        finalSol.append(sol)
+	    if l > 0:
+	        self.parenthesisRecursive(l-1, r , sol+'(' , finalSol)
+	    if r > 0:
+	        self.parenthesisRecursive(l, r-1 , sol+')' , finalSol)
 
 
 sol = Solution()
 start = time.time()
-test = sol.generateParenthesis(15)
+test = sol.generateParenthesis(5)
 end = time.time()
 
 #print test
