d508a28e29847b53756cdcc511146a2aaa771774
JaySurplus <znznbest2004@gmail.com>
2015-12-23 01:01:58 -0600

    Generate Praentheses is Done.

---
+++ python/generateParentheses.py
@@ -0,0 +1,46 @@
+"""
+	Generate Parentheses
+
+	Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+
+	For example, given n = 3, a solution set is:
+
+	"((()))", "(()())", "(())()", "()(())", "()()()"
+
+
+	https://leetcode.com/problems/generate-parentheses/
+"""
+
+import time
+
+
+
+class Solution:
+    # @param an integer
+    # @return a list of string
+    # @draw a decision tree when n == 2, and you can understand it!
+    def helpler(self, l, r, item, res):
+        if r < l:
+            return
+        if l == 0 and r == 0:
+            res.append(item)
+        if l > 0:
+            self.helpler(l - 1, r, item + '(', res)
+        if r > 0:
+            self.helpler(l, r - 1, item + ')', res)
+    
+    def generateParenthesis(self, n):
+        if n == 0:
+            return []
+        res = []
+        self.helpler(n, n, '', res)
+        return res		
+
+
+sol = Solution()
+start = time.time()
+test = sol.generateParenthesis(15)
+end = time.time()
+
+#print test
+print 'running time is %f s' % (end -start)
