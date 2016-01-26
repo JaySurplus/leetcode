1dd92120647419ea4fb31bccd46b92b3b5bea00f
JaySurplus <znznbest2004@gmail.com>
2016-01-26 04:45:57 -0600

    Init problem 93

---
+++ python/93_Restore_IP_Addresses.py
@@ -0,0 +1,22 @@
+"""
+	93. Restore IP Addresses
+
+	Given a string containing only digits, restore it by returning all possible valid IP address combinations.
+
+	For example:
+	Given "25525511135",
+
+	return ["255.255.11.135", "255.255.111.35"]. (Order does not matter)
+"""
+
+class Solution(object):
+    def restoreIpAddresses(self, s):
+        """
+        :type s: str
+        :rtype: List[str]
+        """
+        if len(s) > 12 or len(s) < 4:
+        	return []
+
+        
+
