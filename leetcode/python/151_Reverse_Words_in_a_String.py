f873b28cca410ecc1563ce48704fa55de42cdc88
JaySurplus <znznbest2004@gmail.com>
2016-04-27 12:07:13 -0500

    Problem 151 update.

--- leetcode/python/151_Reverse_Words_in_a_String.py
+++ leetcode/python/151_Reverse_Words_in_a_String.py
@@ -1,4 +1,6 @@
+#!/usr/local/bin/python3
 """
+
 	151. Reverse Words in a String
 
 	Given an input string, reverse the string word by word.
@@ -17,44 +19,71 @@ class Solution(object):
         :type s: str
         :rtype: str
         """
+        slist = []
+
+        pre = " "
+        for i in range(len(s) - 1, -1, -1):
+            if s[i] != " " or s[i] == " " and pre != " ":
+                slist.append(s[i])
+                pre = s[i]
+
+        l = len(slist)
+        j = 0
+        for i in range(l + 1):
+            if i == l or slist[i] == ' ':
+                if i < l - 1 and slist[i + 1] == ' ':
+                    continue
+                k = j
+                while k < (i + j) / 2:
+                    temp = slist[k]
+                    slist[k] = slist[i - 1 - k + j]
+                    slist[i - 1 - k + j] = temp
+                    k += 1
+                j = i + 1
+
+        return ''.join(slist)
+
+    def reverseWordsII(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
         slist = s
         l = len(slist)
         i = 0
         temp = []
         while i < l:
-        	if slist[i] != ' ':
-        		j = i + 1
-        		while j < l and slist[j] != ' ' :
-        			j += 1
-        		temp.append(''.join(slist[i:j]))
-        		i = j
-        	i += 1
-    
+            if slist[i] != ' ':
+                j = i + 1
+                while j < l and slist[j] != ' ':
+                    j += 1
+                temp.append(''.join(slist[i:j]))
+                i = j
+            i += 1
+
         lt = len(temp)
-        for i in xrange(lt/2):
-        	t = temp[i]
-        	temp[i] = temp[lt-1-i]
-        	temp[lt-1-i] = t
+        for i in range(lt / 2):
+            t = temp[i]
+            temp[i] = temp[lt - 1 - i]
+            temp[lt - 1 - i] = t
         return ' '.join(temp)
 
 
 def main(argv=None):
     sol = Solution()
-      
+
     if argv is None:
         if len(sys.argv) == 2:
-        	test = sys.argv[1]
+            test = sys.argv[1]
         else:
-        	test = "this is a test"
-        	test = "the sky is blue"
-        	
-        	
+            test = "this is a test"
+            #test = " "
+
     else:
         test = argv
 
-    
-    print "test case : %s" % test
+    print ("test case : %s" % test)
     res = sol.reverseWords(test)
-    return res , len(res)
+    return res, len(res)
 if __name__ == '__main__':
     sys.exit(main())
