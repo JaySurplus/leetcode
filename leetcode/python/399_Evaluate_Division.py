9064ee08dd31652cbcdcab958a3a07d6cb9524b5
JaySurplus <znznbest2004@gmail.com>
2016-10-03 23:11:38 -0500

    Leetcode 399 done.

---
+++ leetcode/python/399_Evaluate_Division.py
@@ -0,0 +1,20 @@
+"""
+    399 Evaluate Division
+"""
+
+from collections import defaultdict
+class Solution(object):
+    def calcEquation(self, equations, values, queries):
+        """
+        """
+        comfirmed = defaultdict(lambda: float)
+        temp = []
+        e_v = zip(eqautions, values)
+
+
+
+
+
+if __name__ == '__main__':
+    sol = Solution()
+
